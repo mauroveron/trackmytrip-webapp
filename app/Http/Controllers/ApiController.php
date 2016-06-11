93a1c74d920613c42f7f55b7230b84bea9d0fe59
Yif Swery <yiftachswr@gmail.com>
2016-06-11 14:46:08 +1200

    Add saving logic for the entries sent to the API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\LocationEntry;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -17,5 +18,17 @@ class ApiController extends Controller
     public function saveLocations()
     {
         return $this->request->all();
+
+        $inputEntries = [];
+        foreach ($inputEntries as $inputEntry) {
+            $entry = new LocationEntry();
+            $locationEntry->longitude         = $inputEntry->longitude;
+            $locationEntry->latitude          = $inputEntry->latitude;
+            $locationEntry->taken_at          = $inputEntry->timestamp;
+            $locationEntry->device_identifier = $inputEntry->device_identifier;
+            $locationEntry->save();
+        }
+
+        return ['status' => 'ok', 'message' => 'Successfully saved ' . count($inputEntries) . ' location entries'];
     }
 }
