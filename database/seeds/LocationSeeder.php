3addf120e7e389f78fea1d0d5c3f35e0d371e9a0
Yif Swery <yiftachswr@gmail.com>
2016-06-11 14:01:12 +1200

    Create the seeder for the random entries we want in iceland

--- database/seeds/LocationSeeder.php
+++ database/seeds/LocationSeeder.php
@@ -11,6 +11,15 @@ class LocationSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker\Factory::create();
+
+        foreach (range(1, 100) as $item) {
+            $locationEntry                    = new \App\Models\LocationEntry();
+            $locationEntry->longitude         = $faker->longitude(-24, -13); // Iceland Max longitude range
+            $locationEntry->latitude          = $faker->latitude(63.5, 66); // Iceland Max latitude range
+            $locationEntry->taken_at          = $faker->dateTimeThisMonth($max = 'now');
+            $locationEntry->device_identifier = rand(1, 3); // one of three random 'devices'
+            $locationEntry->save();
+        }
     }
 }
